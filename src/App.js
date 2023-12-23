3f1a2035d53cdc434ae87fd52dd5c9f34a964459
Chess-25 <1572484160@qq.com>
2023-12-23 21:48:57 +0800

    添加国际化改中文

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { memo } from "react";
+import { ConfigProvider } from "antd";
 import { Provider } from "react-redux";
 import { renderRoutes } from "react-router-config";
 import { HashRouter } from "react-router-dom";
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import 'moment/locale/zh-cn';//国际化改中文
 import routes from "./router";
 import store from "@/store";
 const App = memo(() => {
   return (
     <Provider store={store}>
-      <HashRouter>
-        {renderRoutes(routes)}
-      </HashRouter>
+      <ConfigProvider locale={zh_CN}>
+        <HashRouter>
+          {renderRoutes(routes)}
+        </HashRouter>
+      </ConfigProvider>
     </Provider>
   );
 });
